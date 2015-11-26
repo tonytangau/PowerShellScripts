6e5022ef2e33b486f7d8f04bae9ddc9ff6a93721
Tony Tang <tony.tang@imdexlimited.com>
2015-11-26 10:31:59 +0800

    Initial check in

---
+++ PowerShell/Hub3DBCreate.ps1
@@ -0,0 +1,22 @@
+﻿$SubscriptionName="Microsoft Partner Network"
+
+Add-AzureAccount
+
+Select-AzureSubscription -SubscriptionName $SubscriptionName
+
+$db_user = "user"
+$db_password = ConvertTo-SecureString -String "pwd" -AsPlainText -Force
+$serverName = "server"
+
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $db_user, $db_password
+
+#create a connection context
+$serverContext = New-AzureSqlDatabaseServerContext -ServerName $serverName -Credential $credential
+
+#Get-AzureSqlDatabase $serverContext –DatabaseName “HubShard1”
+
+Remove-AzureSqlDatabase $serverContext –DatabaseName “HubShard1”
+
+$S0 = Get-AzureSqlDatabaseServiceObjective $serverContext -ServiceObjectiveName "S0"
+
+New-AzureSqlDatabase $serverContext –DatabaseName “HubShard1” -ServiceObjective $S0
---
+++ PowerShell/MoveTenantData.ps1
@@ -0,0 +1,38 @@
+﻿$db_user = "user"
+$db_password = "pwd"
+$serverFrom = "server1"
+$serverTo = "server2"
+$databaseFrom = "Shard1"
+$databaseTo = "Shard2"
+
+$tenantId = "guid-1234"
+
+$TableNames = ("dbo.Table1", "dbo.Table2")
+
+# Copy to databaseTo
+foreach($d in $TableNames){
+
+    $dbQuery = "select * from $d where TenantId = '$tenantId'";
+
+    $dataTable = Invoke-Sqlcmd -ServerInstance $serverFrom -Database $databaseFrom -Username $db_user -Password $db_password -Query $dbQuery;
+
+    $connectionString = "Server = $serverTo; Database = $databaseTo; User ID = $db_user; Password = $db_password;"
+
+    $bulkCopy = new-object ("Data.SqlClient.SqlBulkCopy") $connectionString;
+    $bulkCopy.DestinationTableName = $d;
+
+    if ($dataTable -ne $null)
+    {
+        $bulkCopy.WriteToServer($dataTable);
+    }
+}
+
+# Remove from databaseFrom
+[array]::Reverse($TableNames)
+
+foreach($d in $TableNames){
+
+    $deleteQuery = "delete from $d where TenantId = '$tenantId'";
+
+    $dataTable = Invoke-Sqlcmd -ServerInstance $serverFrom -Database $databaseFrom -Username $db_user -Password $db_password -Query $deleteQuery;
+}
